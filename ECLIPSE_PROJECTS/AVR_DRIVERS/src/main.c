d388929550805fba1f18bbc82335651ae74b2ca0
Ahmed Hassan <ahmadmhasann@gmail.com>
2023-09-16 14:04:09 +0300

    Dio Example 2

--- ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
@@ -9,26 +9,25 @@
 #include "Dio.h"
 
 #define LED_PIN         DIO_PORTA,DIO_PIN0
-#define SWITCH_PIN      DIO_PORTA,DIO_PIN0
+#define SWITCH_PIN      DIO_PORTA,DIO_PIN1
 
 
 int main (void)
 {
+    Dio_LevelType currentState = DIO_HIGH;
+    Dio_LevelType lastState = DIO_HIGH;
+    
     /* Initialization */
     Dio_SetPinMode(LED_PIN, DIO_MODE_OUTPUT);
     Dio_SetPinMode(SWITCH_PIN, DIO_MODE_INPUT_PULLUP);
     while (1)
     {
-        /* Switch is pressed */
-        if (DIO_LOW == Dio_ReadPinLevel(SWITCH_PIN))
+        currentState = Dio_ReadPinLevel(SWITCH_PIN);
+        if ((DIO_LOW == currentState) && (DIO_HIGH == lastState))
         {
-            Dio_SetPinLevel(LED_PIN, DIO_HIGH);
+            Dio_FlipPinLevel(LED_PIN);
         }
-        /* Switch is not pressed */
-        else 
-        {
-            Dio_SetPinLevel(LED_PIN, DIO_LOW);
-        }
+        lastState = currentState;
     }
 
 }
