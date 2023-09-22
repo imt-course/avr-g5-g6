8bfde5ae94959bcbd7d2ef50cb2d895a147f91a7
Ahmed Hassan <ahmadmhasann@gmail.com>
2023-09-22 16:16:39 +0300

    Add LCD more functions

--- ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
@@ -13,11 +13,21 @@
 
 int main (void)
 {
+    u8 count = 0;
     Lcd_Init(&Lcd_Configuration);
-    Lcd_DisplayString("Ahmed ");
+    Lcd_DisplayString("My name is Ahmed Hassan");
+    Lcd_SetCursorPosition(1,5);
+    Lcd_DisplayString("Thank you --- ");
     while (1)
     {
-        
+        _delay_ms(500);
+        Lcd_ShiftDisplayLeft(1);
+        count++;
+        if (count > 8)
+        {
+            Lcd_ReturnHome();
+            count = 0;
+        }
     }
     
 }
