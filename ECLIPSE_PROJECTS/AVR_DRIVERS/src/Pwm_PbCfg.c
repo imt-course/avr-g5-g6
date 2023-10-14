4b9da83138db66d574593056555099c19bf9d39c
Ahmed Hassan <ahmadmhasann@gmail.com>
2023-10-14 16:56:49 +0300

    Electronic Switches

--- ECLIPSE_PROJECTS/AVR_DRIVERS/src/Pwm_PbCfg.c
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/Pwm_PbCfg.c
@@ -10,11 +10,11 @@
 Pwm_ConfigurationType Pwm_Configuration = 
 {
     /* Channel OC0 */
-    .OC0_mode = PWM_MODE_FAST,
+    .OC0_mode = PWM_MODE_OFF,
     .OC0_clk  = PWM_CLK_PRESCALER_8,
     .OC0_out  = PWM_OUT_CLR_THEN_SET,
     /* Channel OC1 */
-    .OC1_mode = PWM_MODE_OFF,
+    .OC1_mode = PWM_MODE_FAST_ICR1,
     .OC1_clk  = PWM_CLK_PRESCALER_8,
     .OC1A_out = PWM_OUT_CLR_THEN_SET,
     .OC1B_out = PWM_OUT_DISCONNECTED,
--- ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
+++ ECLIPSE_PROJECTS/AVR_DRIVERS/src/main.c
@@ -24,14 +24,43 @@
 
 int main (void)
 {
-    Dio_SetPinMode(DIO_PORTA, DIO_PIN0, DIO_MODE_OUTPUT);
-    Dio_SetPinLevel(DIO_PORTA, DIO_PIN0, DIO_HIGH);
-    _delay_ms(1000);
-    Dio_SetPinLevel(DIO_PORTA, DIO_PIN0, DIO_LOW);
-    Wdt_Refresh(WDT_TIMEOUT_1000_MS);
+    /*Dio_SetPinMode(DIO_PORTA, DIO_PIN1, DIO_MODE_OUTPUT);
+    Dio_SetPinMode(DIO_PORTA, DIO_PIN2, DIO_MODE_OUTPUT);
+    Dio_SetPinMode(DIO_PORTA, DIO_PIN3, DIO_MODE_OUTPUT);
+    Dio_SetPinMode(DIO_PORTA, DIO_PIN4, DIO_MODE_OUTPUT);
     while (1)
     {
-        _delay_ms(1100);
-        Wdt_Refresh(WDT_TIMEOUT_1000_MS);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN1, DIO_HIGH);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN2, DIO_LOW);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN3, DIO_LOW);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN4, DIO_HIGH);
+        _delay_ms(1000);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN1, DIO_LOW);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN2, DIO_HIGH);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN3, DIO_HIGH);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN4, DIO_LOW);
+        _delay_ms(1000);
+    }*/
+    /*
+    Dio_SetPinMode(DIO_PORTA, DIO_PIN1, DIO_MODE_OUTPUT);
+    while (1)
+    {
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN1, DIO_HIGH);
+        _delay_ms(1000);
+        Dio_SetPinLevel(DIO_PORTA, DIO_PIN1, DIO_LOW);
+        _delay_ms(1000);
     }
+    */
+   
+    u16 value = 0;
+    Pwm_Init(&Pwm_Configuration);
+    while (1)
+    {
+        Pwm_SetTimeOn(PWM_OUT_OC1A, value / 2);
+        Pwm_SetICR1(value);
+        value += 1000;
+        _delay_ms(1000);
+    }
+    
+    
 }
